32d2ca1406beaa21e988f05b55147c53c5b772ba
Eugenia Smith <ev-genius4@mail.ru>
2024-06-13 11:49:28 +0200

    Local storage interaction added

--- scripts/script_http.js
+++ scripts/script_http.js
@@ -3,7 +3,7 @@ const taskTitleInput = document.querySelector(".task_description");
 const taskDate = document.querySelector(".task_date");
 const taskContainer = document.querySelector(".task_list");
 
-function renderTask() {
+function renderTask(taskElement) {
   console.log(taskTitleInput.value);
   console.log(taskDate.value);
 
@@ -12,8 +12,8 @@ function renderTask() {
   const taskTitle = document.createElement("h3");
   const taskData = document.createElement("p");
 
-  taskTitle.textContent = taskTitleInput.value;
-  taskData.textContent = taskDate.value;
+  taskTitle.textContent = taskElement.title;
+  taskData.textContent = taskElement.date;
 
   checkbox.setAttribute("type", "checkbox");
 
@@ -42,3 +42,48 @@ const taskTitle = //какой-то заголовок задачи
       taskTitle.style.textDecoration = "none";
     }
   });
+
+//логика взаимодействия с задачами в localStorage
+// данные хранятся в виде массива объектов, где каждая задача -- объект
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+if (tasks.length !== 0) {
+  tasks.forEach((task) => {
+    renderTask(task);
+  });
+}
+
+if (tasks.length !== 0) {
+  tasks.forEach((task) => {
+    const liContainer = document.createElement("li");
+    const dateText = document.createElement("p");
+    const heading = document.createElement("h3");
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+
+    liContainer.append(checkbox, heading, dateText);
+
+    dateText.textContent = task.date;
+    heading.textContent = task.title;
+
+    taskContainer.append(liContainer);
+  });
+}
+
+// или воспользоваться функцией, написанной ранее
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newTask = {
+    id: Math.random(),
+    title: taskTitleInput.value,
+    date: taskDate.value,
+    completed: false,
+  };
+
+  renderTask(newTask);
+
+  tasks.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+});
