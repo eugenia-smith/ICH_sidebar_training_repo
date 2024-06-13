b2bc63c28f9d857f91172e27fdd39284f2949940
Eugenia Smith <ev-genius4@mail.ru>
2024-06-13 09:36:38 +0200

    Form submit handled

--- scripts/script_http.js
+++ scripts/script_http.js
@@ -2,3 +2,23 @@ const taskForm = document.querySelector(".task_form");
 const taskTitleInput = document.querySelector(".task_description");
 const taskDate = document.querySelector(".task_date");
 const taskContainer = document.querySelector(".task_list");
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log(taskTitleInput.value);
+  console.log(taskDate.value);
+
+  const liContainer = document.createElement("li");
+  const checkbox = document.createElement("input");
+  const taskTitle = document.createElement("h3");
+  const taskData = document.createElement("p");
+
+  taskTitle.textContent = taskTitleInput.value;
+  taskData.textContent = taskDate.value;
+
+  checkbox.setAttribute("type", "checkbox");
+
+  liContainer.append(checkbox, taskTitle, taskData);
+  taskContainer.append(liContainer);
+});
