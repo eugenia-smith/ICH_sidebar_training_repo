cc9d67178ba7d2a46db61c2b8ad6423554f43dbb
Eugenia Smith <ev-genius4@mail.ru>
2024-07-11 14:53:23 +0200

    submenu functionality added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,9 @@
 const openBtn = document.querySelector(".open_btn");
 const closeBtn = document.querySelector(".close_btn");
 const sideNav = document.querySelector(".side_nav");
+const menuItem = document.querySelectorAll(".nav_option");
+// const submenuItem = document.querySelectorAll(".submenu_option");
+// const submenuContainer = document.querySelector(".submenu_container");
 
 openBtn.addEventListener("click", () => {
   sideNav.style.left = "0px";
@@ -9,3 +12,10 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sideNav.style.left = "-350px";
 });
+
+for (let item of menuItem) {
+  item.addEventListener("click", (event) => {
+    const wrapper = event.target.parentElement;
+    wrapper.children[1].classList.toggle("hidden");
+  });
+}
